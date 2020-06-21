f6318550c375b8bc007a351a3748e5a46ead4e12
unknown <luozhiping@luozhiping-PC.chinastock.com.cn>
2020-06-21 18:10:19 +0800

    first commit

---
+++ src/comp/blog.vue
@@ -0,0 +1,22 @@
+﻿
+export default Vue.component('blog', {
+template: '<div>大家好,我是罗治平</div>',
+data: function() {
+return {
+
+}
+},
+mounted() {
+
+},
+methods: {
+},
+watch: {
+
+}
+
+})
+
+
+
+
---
+++ src/comp/blog2.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <div class="blog">
+    大家好我是罗治平
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style>
+  .el-header {
+  background-color: #409EFF;
+  color: white;
+  }
+  .el-footer {
+  background-color: #909399;
+  color: black;
+  text-align: center;
+  }
+  .el-aside {
+  background-color: chartreuse;
+  }
+  .el-main {
+  background-color: darkkhaki;
+  }
+  .el-col {
+  height: 200px;
+  }
+  .col1 {
+  background-color: teal;
+  }
+  .col2 {
+  background-color: tomato;
+  }
+  .col3 {
+  background-color: thistle;
+  }
+</style>
